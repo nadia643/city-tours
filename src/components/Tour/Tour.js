5d37730446df73ce6ef3b83b9360f3ebe2797c1d
John Doe <nadia643@gmail.com>
2020-02-02 18:17:09 +0000

    added name, city and info

--- src/components/Tour/Tour.js
+++ src/components/Tour/Tour.js
@@ -12,7 +12,15 @@ export default class Tour extends Component {
                     <span className="close-btn">
                         <i className="fas fa-window-close" />
                     </span>
-
+                </div>
+                <div className="tour-info">
+                    <h3>{city}</h3>
+                    <h4>{name}</h4>
+                    <h5>info {""}
+                        <span> 
+                            <i className="fas fa-caret-square-down" />
+                        </span>
+                    </h5>
                 </div>
 
             </article>
