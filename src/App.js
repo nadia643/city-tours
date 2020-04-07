cc0b5a6c9e033623259bc15827180a2187636833
John Doe <nadia643@gmail.com>
2020-04-07 13:30:16 +0100

    made links in navbar <link> and work with app.js

--- src/App.js
+++ src/App.js
@@ -2,16 +2,25 @@ import React from 'react';
 import './App.scss';
 import Navbar from "./components/Navbar/Navbar";
 import TourList from './components/TourList';
+import About from './components/About/About';
+import Home from './components/Home/Home';
+import { Route, BrowserRouter, Switch } from 'react-router-dom';
 
-class App extends React.Component {
-  render() {
+
+function App() {
     return (
       <React.Fragment>
-        <Navbar />
-        <TourList />
-      </React.Fragment>
+      <BrowserRouter> 
+      <Navbar />
+      <Switch> 
+      <Route exact path="/home" component={Home} />
+      <Route exact path="/about" component={About} />
+      <Route exact path="/tour-list" component={TourList} />
+      </Switch>
+      </BrowserRouter>
+    </React.Fragment>
     )
+
   }
-}
 
 export default App;
--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -1,26 +1,19 @@
 import React from 'react';
 import logo from '../../logo.svg';
 import './navbar.scss';
+import { Link } from 'react-router-dom';
+
 
 export default function Navbar() {
     return(
         <nav className="navbar">
             <img src={logo} alt="city tours company" />
             <ul className="nav-links">
-                <li><a href="/" className="nav-link">
-                    Home
-                </a></li>
-                <li><a href="/" className="nav-link">
-                    About
-                </a></li>
-                <li><a href="/" className="nav-link active">
-                    Tours
-                </a></li>
+                <li><Link to="/home" className="nav-link" >Home</Link></li>
+                <li><Link to="/about" className="nav-link">About</Link></li>
+                <li><Link to="/tour-list" className="nav-link">Tour List</Link></li>
             </ul>
-
         </nav>
-
-
 
     )
 }
