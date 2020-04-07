1ec63abf0a96dc191d8c2fe78414e01c2f47f1bb
John Doe <nadia643@gmail.com>
2020-04-07 13:54:44 +0100

    updated background colour accross all 3 pages

--- src/components/About/About.js
+++ src/components/About/About.js
@@ -1,12 +1,18 @@
-import React, { Component } from 'react'
+import React, { Component } from 'react';
+import './About.scss';
 
 export default class About extends Component {
     render() {
         return (
             <div> 
             <h1>
-                hi from about
+                About us
             </h1>
+
+            <p>
+                Founded in 1997, we are here to provide you with tour information
+                from destinations around the world!
+            </p>
             </div>
         )
     }
--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import './Home';
+import './Home.scss';
 
 
 export default class Home extends Component {
